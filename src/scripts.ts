b6e62b45f82673d3a119eed24808808f51dbc9e1
dzelmerichards <dzelme.richards@gmail.com>
2022-04-13 16:29:27 +0300

    add corrections

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,35 +1,19 @@
-const uniteArrays = (a:Array, b:Array):Array => {
-    const arr = [...a, ...b];
-    return arr;
-};
+const uniteArrays = (a: number[], b: number[]): number[] => [...a, ...b];
 
 console.log(uniteArrays([1, 2], [3, 4, 5, 6]));
 
-const joinArrStr = (a:Array, b:string):Array => {
-    const arr = [...a, b];
-    return arr;
-};
+const joinArrStr = (a: string[], b: string): string[] => [...a, b];
 
 console.log(joinArrStr(['Apple', 'Orange', 'Banana'], 'Kiwi'));
 
-const joinStrArr = (a:string, b:Array):Array => {
-    const arr = [b, ...a];
-    return arr;
-};
+const joinStrArr = (a: string, b: string[]): string[] => [b, ...a];
 
 console.log(joinStrArr(['Apple', 'Orange', 'Banana'], 'Kiwi'));
 
-const joinObj = (a:object, b:object):object => {
-    const obj = {...a, ...b};
-    return obj;
-};
+const joinObj = (a: object, b: object): object => ({ ...a, ...b });
 
-console.log(joinObj({ a:1, b:2 }, { c:3, d:4, e:5, f:6 }));
+console.log(joinObj({ a: 1, b: 2 }, { c: 3, d: 4, e: 5, f: 6 }));
 
-const joinObjStr = (a:object, b:string):object => {
-    const obj = {...a};
-    obj.favoriteMovie = b
-    return obj;
-};
+const joinObjStr = (a: object, b: string): object => ({ ...a, favoriteMovie: b });
 
 console.log(joinObjStr({ eyeColor: 'blue', age: 15 }, 'Twilight'));
